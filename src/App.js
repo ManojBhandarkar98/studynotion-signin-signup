34a4d9551d93ab43b050e36fc4997dc730df7e76
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-13 22:03:20 +0530

    Epi - 09 - Studynotion - created components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import { Signup } from "./components/Signup";
+import Dashboard from "./components/Dashboard";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <div>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
