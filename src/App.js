260041d13682bb17f4c7787687a7d2546c7bbca9
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-13 22:17:17 +0530

    Epi - 09 - Studynotion - basis on isLoggedIn boolean it show/hide functionality like false -> login,signup after true-> show logout, dashboard button

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import { Signup } from "./components/Signup";
 import Dashboard from "./components/Dashboard";
+import { useState } from "react";
 
 function App() {
+  const [isLoggedIn,setIsLoggedIn] = useState(false);
   return (
     <div>
       <div>
-        <Navbar/>
+        <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/login" element={<Login/>}/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/Logo.svg'
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const isLoggedIn = props.isLoggedIn;
+    const setIsLoggedIn = props.setIsLoggedIn;
     return (
-        <div className='flex'>
+        <div className='flex justify-evenly'>
             <Link to="/">
                 <img src={logo} alt='logo' width={160} height={32} loading='lazy'></img>
             </Link>
@@ -21,8 +23,27 @@ const Navbar = () => {
                     </li>
                 </ul>
             </nav>
-            <div>
-                
+            <div className='flex ml-5 mr-3 gap-3'>
+                {!isLoggedIn &&
+                    <Link to="/login" >
+                        <button>Login</button>
+                    </Link>
+                }
+                {!isLoggedIn &&
+                    <Link to="/signup" >
+                        <button>Sign Up</button>
+                    </Link>
+                }
+                {isLoggedIn &&
+                    <Link to="/" >
+                        <button>Log Out</button>
+                    </Link>
+                }
+                {isLoggedIn &&
+                    <Link to="/dashboard" >
+                        <button>Dashboard</button>
+                    </Link>
+                }
             </div>
         </div>
     )
