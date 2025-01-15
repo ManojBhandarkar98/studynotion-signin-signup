7e211531f7fb86b1aae399687ee9fcd7d48388fb
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-15 09:15:02 +0530

    Epi - 09 - Studynotion - Signup Page component created - template props send

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import signupImg from "../assets/signup.png"
 
 export const Signup = () => {
   return (
-    <div>Signup</div>
+    <template
+      title='Joins the millions learning to code with StudyNotion for free'
+      desc1='Build skills for today, tommarow, and beyond.'
+      desc2='Education to future-proof your career.'
+      image={signupImg}
+      formType='signup'
+      setIsLoggedIn={setIsLoggedIn}
+    />
   )
 }
